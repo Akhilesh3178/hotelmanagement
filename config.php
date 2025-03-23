9c475d0abc97dd98063acccb33338412c78e22e6
Akhilesh singh <akhileshsingh355616@gmail.com>
2025-03-23 16:18:31 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$server = "localhost";
+$username = "bluebird_user";
+$password = "password";
+$database = "bluebirdhotel";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
+}
+?>
